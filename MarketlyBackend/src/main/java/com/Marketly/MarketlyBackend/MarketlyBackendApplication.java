58860240ab8cf6207f5d4e9e7f9ed938b8627cf8
ravikant <ravikantyadav3243@gmail.com>
2025-08-22 00:34:07 +0530

    starts the project with a gitignore

---
+++ MarketlyBackend/src/main/java/com/Marketly/MarketlyBackend/MarketlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Marketly.MarketlyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketlyBackendApplication.class, args);
+	}
+
+}
---
+++ MarketlyBackend/src/test/java/com/Marketly/MarketlyBackend/MarketlyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Marketly.MarketlyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketlyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
