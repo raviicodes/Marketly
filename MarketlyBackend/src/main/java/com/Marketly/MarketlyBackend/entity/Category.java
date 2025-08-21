cd6a577b18f2f7af305d15d7f6f09d3b54ae981e
ravikant <ravikantyadav3243@gmail.com>
2025-08-22 01:11:08 +0530

    creates the category controller and entity for the product.

---
+++ MarketlyBackend/src/main/java/com/Marketly/MarketlyBackend/entity/Category.java
@@ -0,0 +1,22 @@
+package com.Marketly.MarketlyBackend.entity;
+
+public class Category {
+    public  int categoryId;
+    public String categoryName;
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+}
