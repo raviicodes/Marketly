b47a8efd993582435d79de69986457eba36bce1f
ravikant <ravikantyadav3243@gmail.com>
2025-08-22 23:25:24 +0530

    creates the basic controller service architecture for the product category and implements the get and add category rest apis

--- MarketlyBackend/src/main/java/com/Marketly/MarketlyBackend/controller/CategoryController.java
+++ MarketlyBackend/src/main/java/com/Marketly/MarketlyBackend/controller/CategoryController.java
@@ -1,8 +1,27 @@
 package com.Marketly.MarketlyBackend.controller;
 
+import com.Marketly.MarketlyBackend.entity.Category;
+import com.Marketly.MarketlyBackend.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class CategoryController {
+    @Autowired
+       CategoryService categoryService;
+      @GetMapping("/api/public/categories")
+      public List<Category> getAllCategoryies(){
+           return categoryService.getAllCategories();
+      }
+      @PostMapping("/api/admin/category")
+      public String addCategory(@RequestBody Category category){
+                       return categoryService.addCategory(category);
+      }
 
 }
