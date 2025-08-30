e5f7c55be8a765f7f0804991681353ebf5538650
Ravikant Yadav <101169995+raviicodes@users.noreply.github.com>
2025-08-30 19:00:12 +0530

    Revert "Product module"

--- MarketlyBackend/src/main/java/com/Marketly/MarketlyBackend/service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package com.Marketly.MarketlyBackend.service;
-
-import com.Marketly.MarketlyBackend.payload.ProductDTO;
-import com.Marketly.MarketlyBackend.payload.ProductResponseDTO;
-
-public interface ProductService {
-    ProductDTO addProduct(ProductDTO productDTO,Long categoryId);
-    ProductResponseDTO getProducts(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder);
-}
